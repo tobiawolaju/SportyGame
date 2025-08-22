9187cc55cacd242b4af8ad7306d3d2097059d50f
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2025-08-22 01:52:42 +0100

    :)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      outDir: 'dist',
+      outDir: 'build',
       rollupOptions: {
         plugins: [
           // Enable rollup polyfills plugin
